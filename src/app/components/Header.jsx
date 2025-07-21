55266d77a96548eaa23727e07e3b276ce7717879
blakkhwkk <mylove0012211@gmail.com>
2025-07-21 13:22:03 -0700

    Erros fixes

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
     };
 
     return (
-        <header className="bg-white border-b border-gray-200 sticky top-0 z-50 backdrop-blur-sm bg-white/95">
+        <header className=" border-b border-gray-200 sticky top-0 z-50 backdrop-blur-sm bg-white/95">
             <div className="max-w-7xl mx-auto flex items-center justify-between py-3 px-4 lg:px-8">
                 {/* Logo */}
                 <div className="flex items-center gap-2 group cursor-pointer">
